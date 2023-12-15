102a25cf58aec0b8f0348dcc54123e7418e01b43
gin7018 <79063763+gin7018@users.noreply.github.com>
2023-12-15 10:38:54 -0500

    update the model definition

--- SentimentAnalysisModel.py
+++ SentimentAnalysisModel.py
@@ -2,10 +2,11 @@ import torch
 import transformers
 
 
-class SentimentAnalysisModel(torch.nn.Module):
+class SentimentAnalysisModel(transformers.PreTrainedModel):
 
     def __init__(self):
-        super(SentimentAnalysisModel, self).__init__()
+        config = transformers.DistilBertConfig.from_pretrained('distilbert-base-uncased')
+        super(SentimentAnalysisModel, self).__init__(config=config)
         self.bert = transformers.DistilBertModel.from_pretrained('distilbert-base-uncased')
         self.dropout = torch.nn.Dropout(p=0.3)
         self.output = torch.nn.Linear(in_features=768, out_features=3)  # we have three possible classes
